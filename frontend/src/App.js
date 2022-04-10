531a6faaa49a9a848f19e28b758e7136abf56b98
harsh <harshshah29100@gmail.com>
2022-04-11 01:42:17 +0530

    mongo 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreens from "./screens/HomeScreens";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+    <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+                <Route path="/" element={<HomeScreens />} exact />
+                <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+    <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+const ProductScreen = () => {
+  const [product, setProduct] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
+
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'> {/* remove border */}
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={parseInt(product.rating)} text={`${product.numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Price:</strong> ₹{product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Description:</strong> {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup varient='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Price:
+                  </Col>
+                  <Col>
+                    <strong>₹{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Status:
+                  </Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                    Add To Cart
+                  </Button>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>          
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default ProductScreen
