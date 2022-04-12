14cb7ff0d775b4685c3ab15a8981e335ea6850cd
harsh <harshshah29100@gmail.com>
2022-04-12 19:45:17 +0530

    5 2 redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<Provider store={store}><App/></Provider>);
 
 
 
