a6eaa892062ec2a279cb2a6696672df50fba1012
harsh <harshshah29100@gmail.com>
2022-05-29 01:28:25 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,7 @@ app.use(express.json())
 // })
 
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+
 
 app.use('/api/products', productRoutes)
 
@@ -50,6 +48,22 @@ app.get('/get-razorpay-key', (req, res) => res.send({ key:process.env.RAZORPAY_K
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+
+    })
+
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
+
 
 app.use(notFound)
 
